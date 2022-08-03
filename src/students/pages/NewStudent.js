0acfca65f5aedd3f445fba33ab1e16723f7a3e78
ShayanZuberi <i190749@nu.edu.pk>
2022-08-03 19:01:38 +0500

    initial commit

---
+++ src/students/pages/NewStudent.js
@@ -0,0 +1,7 @@
+import React  from "react";
+
+const NewStudent = () => {
+    return(<h2>Add a new Student</h2>);
+};
+
+export default NewStudent;
---
+++ src/teachers/pages/NewTeacher.js
@@ -0,0 +1,7 @@
+import React  from "react";
+
+const NewTeacher = () => {
+    return(<h2>Add a new Teacher</h2>);
+};
+
+export default NewTeacher;
